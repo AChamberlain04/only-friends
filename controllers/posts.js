a0d83174ee8f2a9c34176f97b97151caae46939e
Adam Chamberlain <a.chamberlain04@gmail.com>
2026-08-31 11:20:22 -0400

    Add HEIC image upload support

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,9 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment");
+const heicConvert = require("heic-convert");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -11,6 +14,7 @@ module.exports = {
       console.log(err);
     }
   },
+
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
@@ -19,20 +23,64 @@ module.exports = {
       console.log(err);
     }
   },
+
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-       const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
-      res.render("post.ejs", { post: post, user: req.user, comments: comments });
+      const comments = await Comment.find({ post: req.params.id })
+        .sort({ createdAt: "desc" })
+        .lean();
+
+      res.render("post.ejs", {
+        post: post,
+        user: req.user,
+        comments: comments,
+      });
     } catch (err) {
       console.log(err);
     }
   },
+
   createPost: async (req, res) => {
+    let uploadPath = null;
+    let convertedPath = null;
+
     try {
-      // Upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
-console.log("BODY:", req.body)
+      // Make sure a file was actually uploaded
+      if (!req.file) {
+        throw new Error("No image was uploaded");
+      }
+
+      uploadPath = req.file.path;
+
+      const ext = path.extname(req.file.originalname).toLowerCase();
+
+      // Convert HEIC/HEIF to JPEG
+      if (ext === ".heic" || ext === ".heif") {
+        convertedPath = `${req.file.path}.jpg`;
+
+        const inputBuffer = fs.readFileSync(req.file.path);
+
+        const outputBuffer = await heicConvert({
+          buffer: inputBuffer,
+          format: "JPEG",
+          quality: 0.9,
+        });
+
+        fs.writeFileSync(convertedPath, outputBuffer);
+
+        uploadPath = convertedPath;
+      }
+
+      // Upload image to Cloudinary
+      const result = await cloudinary.uploader.upload(uploadPath, {
+        folder: "only-friends",
+      });
+
+      console.log("BODY:", req.body);
+      console.log("Image uploaded to Cloudinary:", result.secure_url);
+
+      // Create post
       await Post.create({
         title: req.body.title,
         image: result.secure_url,
@@ -41,14 +89,26 @@ console.log("BODY:", req.body)
         likes: 0,
         user: req.user.id,
       });
-      console.log(req.body);
+
       console.log("Post has been added!");
+
       res.redirect("/profile");
     } catch (err) {
-      console.log(err);
-    
+      console.error("CREATE POST ERROR:", err);
+      res.status(500).send("Internal Server Error");
+    } finally {
+      // Delete temporary uploaded file
+      if (req.file?.path && fs.existsSync(req.file.path)) {
+        fs.unlinkSync(req.file.path);
+      }
+
+      // Delete converted JPEG if one was created
+      if (convertedPath && fs.existsSync(convertedPath)) {
+        fs.unlinkSync(convertedPath);
+      }
     }
   },
+
   likePost: async (req, res) => {
     try {
       await Post.findOneAndUpdate(
@@ -57,23 +117,29 @@ console.log("BODY:", req.body)
           $inc: { likes: 1 },
         }
       );
+
       console.log("Likes +1");
       res.redirect(`/post/${req.params.id}`);
     } catch (err) {
       console.log(err);
     }
   },
+
   deletePost: async (req, res) => {
     try {
       // Find post by id
       let post = await Post.findById({ _id: req.params.id });
-      // Delete image from cloudinary
+
+      // Delete image from Cloudinary
       await cloudinary.uploader.destroy(post.cloudinaryId);
-      // Delete post from db
+
+      // Delete post from database
       await Post.remove({ _id: req.params.id });
+
       console.log("Deleted Post");
       res.redirect("/profile");
     } catch (err) {
+      console.log(err);
       res.redirect("/profile");
     }
   },
